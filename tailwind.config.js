0d57517d302e2e8139f2043a82d5ee0bdf567dc5
nitad54448 <ndragoe@gmail.com>
2025-08-25 16:28:48 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}" // 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
